004807da1ef16d175aa9b4e47987148fc360374f
Leonardo Natali <leonardo.natali@hotmail.com>
2021-09-23 05:00:06 -0300

    chore: add comments for Storage Interface

--- pkg/storage/storage.go
+++ pkg/storage/storage.go
@@ -6,11 +6,18 @@ import (
 )
 
 type StorageService interface {
+	// Carrega as configurações
 	Load(cfg *StorageConfig) error
+	// Verifica se o bucket da configuração existe
 	BucketExists() (bool, error)
+	// Cria um bucket com o mesmo nome do bucket da configuração
 	CreateBucket() error
+	// Adiciona o arquivo ao bucket da configuração
 	PutFile(content io.Reader, path, mimeType string, size uint64) error
+	// Remove um arquivo
 	DeleteFile(path string) error
+	// Obtém o Link de download
 	GetDownloadURL(path, filename, mimeType string, expires time.Duration) (string, error)
+	// Move um arquivo de um lugar para o outro
 	Move(src, dest string) error
 }
